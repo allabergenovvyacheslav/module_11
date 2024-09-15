f5ebdf399572eea99d0569a69e55fa6fd8432539
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-09-15 22:08:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+
+
+def new_foto_1(name):
+    image = Image.open(name)
+    w, h = image.size
+    return image.resize((w // 1, h // 1))
+
+
+def new_foto_2(name):
+    image = Image.open(name)
+    w, h = image.size
+    return image.resize((w // 2, h // 2))
+
+
+im = new_foto_1('cat.jpg')
+im2 = new_foto_2('sun.jpg')
+
+im.paste(im2, (200, 200))
+im.show()
